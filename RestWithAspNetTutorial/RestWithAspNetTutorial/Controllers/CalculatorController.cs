e88de66735dd70227e07ba78d125fb900a3e2657
Andressa Neumann <neumm.andressa@gmail.com>
2019-10-15 13:25:15 -0300

    sum function implemented

---
+++ RestWithAspNetTutorial/RestWithAspNetTutorial/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspNetTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculatorController : Controller
+    {
+        // GET api/values/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }                
+
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string number)
+        {
+            bool isNumber = double.TryParse(number, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out double newNumber);
+
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if(decimal.TryParse(number, out decimalValue))            
+                return decimalValue;
+            
+            return 0;
+        }
+    }
+}
