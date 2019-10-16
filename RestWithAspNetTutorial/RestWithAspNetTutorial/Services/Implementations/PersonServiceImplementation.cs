a8586b88db5250a19c8068ddda44b02119b098de
Andressa Neumann <neumm.andressa@gmail.com>
2019-10-16 17:23:34 -0300

    rest verbs implemented and working/connection with database done

--- RestWithAspNetTutorial/RestWithAspNetTutorial/Services/Implementations/PersonServiceImplementation.cs
+++ RestWithAspNetTutorial/RestWithAspNetTutorial/Services/Implementations/PersonServiceImplementation.cs
@@ -1,71 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using RestWithAspNetTutorial.Model;
+using RestWithAspNetTutorial.Model.Context;
 
 namespace RestWithAspNetTutorial.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonService
     {
-        private volatile int count;
+        private MySQLContext _context;
+
+        public PersonServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
 
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
             return person;
         }
 
         public void Delete(long id)
         {
-           
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            try
+            {
+                if (result != null) _context.Persons.Remove(result);
+                _context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public List<Person> FindAll()
         {
-            List<Person> persons = new List<Person>();
-
-            for(int i = 0; i < 8; i++)
-            {
-                Person person = MockPerson(i);
-                persons.Add(person);
-            }
-
-            return persons;
-        }
-
-        private Person MockPerson(int i)
-        {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Person Name " + i,
-                LastName = "Person LastName " + i,
-                Address = "Some Address",
-                Gender = "Female/Male"
-
-            };
-        }
-
-        private long IncrementAndGet()
-        {
-            return Interlocked.Increment(ref count);
+            return _context.Persons.ToList();
         }
 
         public Person FindById(long id)
         {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Andressa",
-                LastName = "Neumann",
-                Address = "General Neto 1035",
-                Gender = "Female"
-            
-            };
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
 
         public Person Update(Person person)
         {
+            if (!Exist(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            try
+            {
+                _context.Entry(result).CurrentValues.SetValues(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
             return person;
+        }
+
+        private bool Exist(long? id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
